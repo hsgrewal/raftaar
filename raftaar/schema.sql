dd20f9765c9603f78ad0032a5bf5a61679d68b71
Harkishan Grewal <harkishangrewal@gmail.com>
2022-12-09 09:31:57 -0800

    Update schema

--- raftaar/schema.sql
+++ raftaar/schema.sql
@@ -1,10 +1,16 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS vehicle;
+DROP TABLE IF EXISTS gas;
+DROP TABLE IF EXISTS loan;
+DROP TABLE IF EXISTS maintenance;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
 );
 
 CREATE TABLE post (
@@ -12,6 +18,48 @@ CREATE TABLE post (
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
-  body TEXT NOT NULL,
+  body TEXT,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE vehicle (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  owner_id INTEGER NOT NULL,
+  vin TEXT NOT NULL,
+  license_plate TEXT,
+  year TEXT,
+  make TEXT,
+  model TEXT,
+  FOREIGN KEY (owner_id) REFERENCES user (id)
+);
+
+CREATE TABLE gas (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  vehicle_id INTEGER NOT NULL,
+  date TEXT NOT NULL,
+  gallons REAL NOT NULL,
+  cost REAL NOT NULL,
+  mileage INTEGER NOT NULL,
+  FOREIGN KEY (vehicle_id) REFERENCES vehicle (id)
+);
+
+CREATE TABLE loan (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  vehicle_id INTEGER NOT NULL,
+  date TEXT NOT NULL,
+  amount REAL NOT NULL,
+  cost REAL NOT NULL,
+  memo TEXT NOT NULL,
+  FOREIGN KEY (vehicle_id) REFERENCES vehicle (id)
+);
+
+CREATE TABLE maintenance (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  vehicle_id INTEGER NOT NULL,
+  date TEXT NOT NULL,
+  cost REAL NOT NULL,
+  mileage INTEGER NOT NULL,
+  memo TEXT NOT NULL,
+  type TEXT NOT NULL,
+  FOREIGN KEY (vehicle_id) REFERENCES vehicle (id)
 );
